20eaadf403dc8f527593aff79eb054548daa40df
Panther Robotics <32471733+blhsrobotics@users.noreply.github.com>
2018-01-13 09:27:44 -0800

    removed useless features from  blob

--- src/org/usfirst/frc/team3218/robot/subsystems/Blob.java
+++ src/org/usfirst/frc/team3218/robot/subsystems/Blob.java
@@ -10,10 +10,6 @@ public class Blob {
 	public float averageY = 0;
 	public float averageWidth = 0;
 	public float averageHeight = 0;
-	public char[] xSamples = new char[Pixy.SAMPLE_COUNT];
-	public char[] ySamples = new char[Pixy.SAMPLE_COUNT];
-	public char[] widthSamples = new char[Pixy.SAMPLE_COUNT];
-	public char[] heightSamples = new char[Pixy.SAMPLE_COUNT];
 	public boolean wasUpdated = false;//this allows you to tell if this blob was updated in the most recent frame
 	public int lastIndex = 0;
 }
